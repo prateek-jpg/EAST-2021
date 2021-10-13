e9007e9759fb692a52c066f66b6747afe1d32be3
pratikash <pratikashpanda.1234@gmail.com>
2021-10-13 11:23:48 +0530

    minor homepage issues updated

--- js/home.js
+++ js/home.js
@@ -5,6 +5,7 @@ slider.classList.remove("show-slider");
 const navToggle = document.querySelector(".nav-toggle");
 const barBtn = document.querySelector(".fas");
 const navLinks = document.querySelector(".navbar-links");
+const stickyHeader = document.querySelector(".sticky-header");
 
 navToggle.addEventListener("click", (e) => {
   slider.classList.toggle("show-slider");
@@ -21,4 +22,12 @@ window.addEventListener("scroll", function () {
   if (slider.classList.contains("show-slider")) {
     slider.classList.remove("show-slider");
   }
+  if (barBtn.classList.contains("fa-times")) {
+    barBtn.classList.remove("fa-times");
+    barBtn.classList.add("fa-bars");
+  }
 });
+
+function myFunc() {
+  stickyHeader.style.background.color = "black";
+}
